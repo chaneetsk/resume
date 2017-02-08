1ab8c881f0eb27b39681499ec8e53496a292c152
Endless River <skz@Endlesss-MacBook-Air.local>
2017-02-08 23:23:01 +1100

    no generator

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -4,8 +4,8 @@ import Summary from './Summary';
 import Experience from './Experience';
 import Education from './Education';
 import Skills from './Skills';
-import Reference from './Reference';
-import Generate from './Generate';
+// import Reference from './Reference';
+// import Generate from './Generate';
 import axios from 'axios';
 
 //import customData from '../data.json';
@@ -18,9 +18,10 @@ export default class AppLayout extends Component {
       content: {}
     };
   }
-  //called the first time the component is loaded right after the compoenent is added to the page
+  /* called the first time the component is loaded
+  right after the compoenent is added to the page */
   componentDidMount = () => {
-    const url = "https://www.sanjeet.com.au/resume/data.json";
+    const url = "./data.json";
     const _this = this;
     axios.get(url)
       .then(function(response){
@@ -46,13 +47,14 @@ export default class AppLayout extends Component {
           tel={this.state.content.tel}
           email={this.state.content.email}
           web={this.state.content.web}
+          linkedIn={this.state.content.linkedIn}
         />
         <Summary heading="Summary" summary={this.state.content.summary}/>
         <Experience heading="Experience" exp={this.state.content.exp}/>
         <Education heading="Education" edu={this.state.content.edu}/>
         <Skills heading="Skills" skills={this.state.content.skills}/>
-        <Reference heading="Reference"/>
-        <Generate data={this.state.content}/>
+        {/*<Reference />*/}
+        {/*}<Generate data={this.state.content}/>*/}
       </div>
     );
   }
